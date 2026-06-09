23105c6e080374f24b893fd21e5337f280c4b847
RizzOtto <smashboy90@gmail.com>
2026-06-09 20:37:26 +0200

    fix: lock UI language to English, remove German locale entirely

--- apps/frontend/src/i18n/index.ts
+++ apps/frontend/src/i18n/index.ts
@@ -1,38 +1,19 @@
 import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import de from './locales/de/common.json';
 import en from './locales/en/common.json';
 
 void i18n
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    lng: 'en',
     fallbackLng: 'en',
-    supportedLngs: ['de', 'en'],
     defaultNS: 'common',
     ns: ['common'],
     resources: {
-      de: { common: de },
       en: { common: en },
-    },
-    detection: {
-      order: ['localStorage'],
-      caches: ['localStorage'],
-      lookupLocalStorage: 'rizzotto_locale',
     },
     interpolation: { escapeValue: false },
     returnNull: false,
   });
-
-i18n.on('languageChanged', (lng) => {
-  if (typeof document !== 'undefined') {
-    document.documentElement.lang = lng;
-  }
-});
-
-if (typeof document !== 'undefined') {
-  document.documentElement.lang = i18n.resolvedLanguage ?? 'en';
-}
 
 export default i18n;
